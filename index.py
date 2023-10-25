ef8584ee81ff9fbc13f2afe1dc75902bc2a3be5b
song <yyysss099@gmail.com>
2023-10-25 23:02:50 +0800

    add headers

--- index.py
+++ index.py
@@ -93,7 +93,15 @@ proxies = {
   'http': proxyurl,
 }
 
-web_data = requests.get(url, proxies=proxies)
+headers = {
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding":"gzip, deflate",
+    "Accept-Language":"zh-CN,zh;q=0.9",
+    "Cache-Control":"max-age=0",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+}
+
+web_data = requests.get(url, proxies=proxies, headers=headers)
 web_data.encoding = 'utf-8'
 print(web_data.text)
 
